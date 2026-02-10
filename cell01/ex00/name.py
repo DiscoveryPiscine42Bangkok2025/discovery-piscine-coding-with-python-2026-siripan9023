6921ad085889d0a68570a102e89468cfc1abafbd
66070251 <itsuchabeautifulnewday@gmail.com>
2026-02-10 21:05:23 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Chaiviwat"
+last_name = "Siripan"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Chaiviwat"
+last_name = "Siripan"
+whole_name = first_name + " " + last_name
+print(whole_name)
